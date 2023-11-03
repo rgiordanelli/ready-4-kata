1857d42c14f00e9af58e2333326f9ed971d1eadc
Raffaele Giordanelli <raffaele.giordanelli@wallstreetfootball.io>
2023-11-03 16:05:21 +0100

    Terzo test

--- Java/app/src/main/java/kata/scaffolding/StringCalculator.java
+++ Java/app/src/main/java/kata/scaffolding/StringCalculator.java
@@ -2,7 +2,9 @@ package kata.scaffolding;
 
 public class StringCalculator {
   public int add(String s) {
-    if(s.isEmpty()) return 0;
+    if (s.isEmpty()) {
+      return 0;
+    }
     return -1;
   }
 }
--- Java/app/src/test/java/kata/scaffolding/StringCalculatorTest.java
+++ Java/app/src/test/java/kata/scaffolding/StringCalculatorTest.java
@@ -3,18 +3,26 @@ package kata.scaffolding;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
   StringCalculator stringCalculator = new StringCalculator();
 
   @Test
   void when_Empty_String_Is_Passed_Then_Zero_Is_Returned() {
     int computedValue = stringCalculator.add("");
-    Assertions.assertEquals(0, computedValue);
+    assertEquals(0, computedValue);
   }
 
   @Test
   void when_One_Figure_Is_Passed_Then_The_Sum_Is_Returned() {
     int computedValue = stringCalculator.add("1");
-    Assertions.assertEquals(1, computedValue);
+    assertEquals(1, computedValue);
   }
+  @Test
+  void when_theInputIsAListOfNumbers_thenTheSumIsReturned() {
+    int computedValue = stringCalculator.add("1,2,3");
+    assertEquals(6, computedValue);
+  }
+
 }
